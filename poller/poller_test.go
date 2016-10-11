bd42989031637251b7aeb7a0df62dcdea1929189
pestophagous <pestophagous@users.noreply.github.com>
2016-10-10 22:20:53 -0700

    My first go test. It actually even fails :)
    go test github.com/pestophagous/hackybeat/poller

---
+++ poller/poller_test.go
@@ -0,0 +1,36 @@
+package poller
+
+import (
+	"testing"
+	"time"
+
+	lpkg "github.com/pestophagous/hackybeat/logger"
+)
+
+type simplePollable struct {
+	counter int
+}
+
+func (this *simplePollable) DoPoll() time.Duration {
+	this.counter++
+	return 0
+}
+
+func (this *simplePollable) OnShutdown() {
+}
+
+func TestPollBasic(t *testing.T) {
+
+	logger := lpkg.NewNoopLogAdapter()
+	pollable := &simplePollable{}
+
+	poller := NewPoller(logger, pollable)
+
+	poller.BeginBackgroundPolling()
+
+	poller.Stop()
+
+	if pollable.counter < 1 {
+		t.Error("Pollable was not polled.")
+	}
+}
